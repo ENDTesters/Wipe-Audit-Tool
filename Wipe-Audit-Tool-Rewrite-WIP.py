121faba2cdbc3dd5d1eb2f699d506e27870bd681
ENDTesters <167399100+ENDTesters@users.noreply.github.com>
2024-04-19 05:48:03 -0400

    Update Wipe-Audit-Tool-Rewrite-WIP.py

--- Wipe-Audit-Tool-Rewrite-WIP.py
+++ Wipe-Audit-Tool-Rewrite-WIP.py
@@ -32,14 +32,57 @@ if 'userLoginCompleted' not in st.session_state:
 
 # Set API variables
 
-#apiAuthLink = "https://api-us-west.securaze.com/api/auth/login"
-apiAuthLink = "placeholder"
+apiAuthLink = "https://api-us-west.securaze.com/api/auth/login"
+#apiAuthLink = "placeholder"
 # Streamlit Page Setup
 
 st.set_page_config(page_title="Securaze Wipe Check & Audit Log Alpha", layout="centered")
 col1, col2 = st.columns([1, 1])
 
 #define login function
+# check to see if the session is logged in yet or not
+try:
+	if st.session_state['loginYesorNoResponse'] == "User successfully logged in.":
+		st.session_state['userLoginCompleted'] = True
+except:
+	st.toast('Could not find a previous successful connection. Check your username and password.')
+
+try:
+	if st.session_state['userLoginCompleted'] == True:
+		st.toast("Login for " + st.session_state['securazeUsername'] + " remembered.")
+		st.info("Latest login response: " + st.session_state['loginYesorNoResponse'], icon=ℹ)
+		#mainLoop()
+	else:
+		st.session_state['securazeUsername'] = st.text_input("Username")
+		st.session_state['securazePassword'] = st.text_input("Password", type="password")
+		if st.button("Login", use_container_width=True):
+			with st.sidebar:
+				try:
+					loginQueryParams = {'Username': st.session_state['securazeUsername'], 'Password': st.session_state['securazePassword'], 'RememberMe': 'True' }
+					securazeAPILogin = req.post(apiAuthLink, data=loginQueryParams)
+					securazeAPILoginDict = securazeAPILogin.json()
+					st.session_state['loginYesorNoResponse'] = securazeAPILoginDict.get('message')
+					loginRequestRaw = securazeAPILoginDict.get('result')
+					loginResponseData = loginRequestRaw.get("data")	
+					try:
+						loginToken = loginResponseData['Token']
+						customerData = loginResponseData.get("Customers")
+						st.session_state['customerZeroData'] = customerData[0]
+						st.session_state['customerName'] = st.session_state['customerZeroData']['Name']
+						st.session_state['customerID'] = st.session_state['customerZeroData']['CustomerID']
+						st.session_state['tokenObtained'] = True
+					except:
+						loginToken = ""
+						customerData = ""
+						st.session_state['customerZeroData'] = ""
+						st.session_state['customerName'] = ""
+						st.session_state['customerID'] = ""
+						st.session_state['tokenObtained'] = ""
+						st.toast("An error occured, but the program will continue to run. Check your login details.")
+				except:
+					pass
+except:
+	pass
 
 def login():
 	try:
@@ -69,25 +112,7 @@ def login():
 	except:
 		pass
 
-# check to see if the session is logged in yet or not
 
-try:
-	if st.session_state['userLoginCompleted'] == True:
-		st.toast("Login for " + st.session_state['securazeUsername'] + " remembered.")
-		st.info("Latest login response: " + st.session_state['loginYesorNoResponse'], icon=ℹ)
-		#mainLoop()
-	else:
-		st.toast("Waiting for Authentication")
-
-		st.session_state['securazeUsername'] = st.text_input("Username")
-		st.session_state['securazePassword'] = st.text_input("Password", type="password")
-		if st.button("Login", use_container_width=True):
-			with st.sidebar:
-				login()
-except:
-	pass
-if st.session_state['loginYesorNoResponse'] == "User successfully logged in.":
-	st.session_state['userLoginCompleted'] = True
 with st.sidebar:
 	if st.button("Reset Login", use_container_width=True):
 		st.session_state['userLoginCompleted'] = False
@@ -108,7 +133,6 @@ if st.session_state['userLoginCompleted'] == False:
 	userLoginCompletedStr = "False"
 else:
 	userLoginCompletedStr = "True"
-st.toast("Login Validated: " + userLoginCompletedStr)
 def mainLoop(): [
 
 ]
